0a6c0119c156c0cd0972cb9e2743deb07526ff18
COMPU-S040\daw1 <pavelmiron6824@gmail.com>
2025-01-10 11:25:23 +0100

    no message

---
+++ DAW/UD3/UD3 – Actividad 04 – Herencia II/AC_4/src/App.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+       
+        Scanner scanner = new Scanner(System.in);
+
+        do { 
+            System.out.println("...");
+
+            String opcion = null;
+            switch (opcion) {
+                case "1":
+                    
+                    break;
+                default:
+                 
+                
+            }
+
+        } while (true);
+
+
+    }
+}
