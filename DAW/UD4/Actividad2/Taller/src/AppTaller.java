aed701cc85ab4e649b0c43b266d383a69a00c2f5
COMPU-S040\daw1 <pavelmiron6824@gmail.com>
2025-01-21 09:04:06 +0100

    no message

--- DAW/UD4/Actividad2/Taller/src/AppTaller.java
+++ DAW/UD4/Actividad2/Taller/src/AppTaller.java
@@ -1,6 +1,53 @@
+import java.util.Scanner;
+
 public class AppTaller {
     public static void main(String[] args) throws Exception {
         
-        
+        Taller taller = new Taller();
+
+        Scanner scanner = new Scanner(System.in);
+
+        String opcion;
+
+        do { 
+            System.out.println("1.Anadir coche");
+            System.out.println("2.Eliminar coche");
+            System.out.println("3.Salir");
+            
+
+            opcion = scanner.nextLine();
+            switch (opcion) {
+                case "1":
+                    System.out.println("Introduce el color del coche: ");
+                    String color = scanner.nextLine();
+
+                    System.out.println("Introduce la marca del coche: ");
+                    String marca = scanner.nextLine();
+
+                    System.out.println("Introduce la matricula del coche: ");
+                    String matricula = scanner.nextLine();
+
+                    Coche coche = new Coche(color, marca);
+                    taller.anadeElemento(matricula, coche);
+
+                    break;
+                    
+                case "2":
+                    System.out.println("Introduce la matricula para eliminar el coche: ");
+                    String eliminarCoche = scanner.nextLine();
+                    taller.eliminaElemento(eliminarCoche);
+                    break;
+
+                case "3":
+                    System.out.println("Hasta luego!");
+                    break;
+                default:
+                 
+                
+            }
+
+        } while (!"3".equals(opcion));
+
+
     }
 }
--- DAW/UD4/Actividad2/Taller/src/Taller.java
+++ DAW/UD4/Actividad2/Taller/src/Taller.java
@@ -12,15 +12,53 @@ public class Taller {
 
     public boolean anadeElemento(String matricula, Coche coche){
         if (cochesEnTaller.containsKey(matricula)) {
-            
-        }
-        return true;
+            System.out.println("Coche con esa matricula ya existe");
+            return false;
+        }else{
+            cochesEnTaller.put(matricula, coche);
+            System.out.println("Coche registardo correcto");
+            return true;
+        }  
     }
 
+    public void eliminaElemento(String matricula){
+        if (cochesEnTaller.containsKey(matricula)) {
+            cochesEnTaller.remove(matricula);
+            System.out.println("Coche eliminado");
+        }else{
+            System.out.println("No existe coche con este matricula");
+        }
+    }
     
- 
+    public void visualizaMatriculas(){
+        if (cochesEnTaller.isEmpty()) {
+            System.out.println("No hay coches en taller");
+        }else{
+            for (String matricula : cochesEnTaller.keySet()) {
+                System.out.println(matricula);
+            }
+        }
+    }
     
-   
+    public void visualizaCoches(){
+        if (cochesEnTaller.isEmpty()) {
+            System.out.println("No hay coches en taller");
+        }else{
+            for (Coche coches : cochesEnTaller.values()) {
+                System.out.println(coches);
+            }
+        }
+    }
     
-    
+    public void visualizaTaller(){
+        if (cochesEnTaller.isEmpty()) {
+            System.out.println("No hay coches en taller");
+        }else{
+            for (String matricula : cochesEnTaller.keySet()) {
+                Coche coches = cochesEnTaller.get(matricula);
+                System.out.println("Matricula =[" + matricula + "] " + "Coche=[" + coches + "]");
+            }
+        }
+    }
+
 }
