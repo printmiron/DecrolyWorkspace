75bd43a117e86e003c85d502b796d7d182bf0287
COMPU-S040\daw1 <pavelmiron6824@gmail.com>
2025-03-21 11:14:24 +0100

    no message

--- BDD/UD4/AC1/AC1.sql
+++ BDD/UD4/AC1/AC1.sql
@@ -2,16 +2,31 @@ DROP DATABASE IF EXISTS ud4_ac1;
 CREATE DATABASE IF NOT EXISTS ud4_ac1;
 USE ud4_ac1;
 
-CREATE	TABLE	Alumno	(
+CREATE	TABLE	alumno	(
 	nombre					VARCHAR	(40)	NOT	NULL,
 	apellido				VARCHAR	(40)	NOT	NULL,
-	fecha_de_nacimiento	 	DATE			NOT NULL
+	f_nac	 				DATE			NOT NULL
 );
 
-INSERT	INTO	Alumno	(nombre, apellido, fecha_de_nacimiento)	VALUES 
+INSERT	INTO	Alumno	(nombre, apellido, f_nac)	VALUES 
 ('Pavel', 'Miron', '2004-08-06'),
 ('Victor', 'Fernandez', '2005-12-05'),
 ('Pepe', 'Grande', '2002-04-26');
 
+DROP	USER	'decroly'@'localhost';
 
-CREATE	USER	'decroly'@'localhost'	IDENTIFIED	BY	'decoly';
+CREATE	USER	'decroly'@'localhost'	IDENTIFIED	BY	'decroly';
+
+GRANT	SELECT	ON	Alumno	TO	'decroly'@'localhost';
+
+GRANT	UPDATE	(f_nac)	ON	Alumno	TO  'decroly'@'localhost';
+
+use ud4_ac1; 
+SELECT * FROM alumno; 
+UPDATE alumno SET f_nac='1990-04-20' WHERE nombre='nombre del alumno'; 
+UPDATE alumno SET apellido='Alonso' WHERE nombre='nombre del alumno'; 
+INSERT INTO alumno VALUES ('Fernando', 'Alonso', '1981-07-29'); 
+
+
+SHOW DATABASES;
+
