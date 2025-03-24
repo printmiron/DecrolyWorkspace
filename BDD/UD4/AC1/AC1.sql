7f3264d0bf983adb40232badfef16149addd72d4
COMPU-S040\daw1 <pavelmiron6824@gmail.com>
2025-03-24 12:48:39 +0100

    no message

--- BDD/UD4/AC1/AC1.sql
+++ BDD/UD4/AC1/AC1.sql
@@ -8,25 +8,43 @@ CREATE	TABLE	alumno	(
 	f_nac	 				DATE			NOT NULL
 );
 
-INSERT	INTO	Alumno	(nombre, apellido, f_nac)	VALUES 
+INSERT	INTO	alumno	(nombre, apellido, f_nac)	VALUES 
 ('Pavel', 'Miron', '2004-08-06'),
 ('Victor', 'Fernandez', '2005-12-05'),
-('Pepe', 'Grande', '2002-04-26');
+('Pepe', 'Grande', '2002-04-26'),
+('Fernando', 'Alonso', '1981-07-29'); 
 
-DROP	USER	'decroly'@'localhost';
+-- GRANT ALL PRIVILEGES ON ud4_ac1.* TO 'usuarios';
 
-CREATE	USER	'decroly'@'localhost'	IDENTIFIED	BY	'decroly';
+-- REVOKE usuarios FROM 'alumno'@'localhost';
 
-GRANT	SELECT	ON	Alumno	TO	'decroly'@'localhost';
 
-GRANT	UPDATE	(f_nac)	ON	Alumno	TO  'decroly'@'localhost';
+-- CREATE ROLE usuarios;
+
+-- GRANT usuarios TO 'decroly'@'localhost';
+-- GRANT usuarios TO 'alumno'@'localhost';
+
+-- GRANT ALL PRIVILEGES ON ud4_ac1. * TO 'decroly'@'localhost';
+
+-- DROP	USER	'alumno'@'localhost';
+
+-- CREATE	USER	'alumno'@'localhost'	IDENTIFIED	BY	'alumno';
+
+-- DROP	USER	'decroly'@'localhost';
+
+-- CREATE	USER	'decroly'@'localhost'	IDENTIFIED	BY	'decroly';
+
+-- GRANT	SELECT	ON	alumno	TO	'decroly'@'localhost';
+
+-- GRANT	UPDATE	(f_nac)	ON	alumno	TO  'decroly'@'localhost';
 
 use ud4_ac1; 
+
+UPDATE alumno SET f_nac='1990-04-20' WHERE nombre='Fernando'; 
+UPDATE alumno SET apellido='Alonso' WHERE nombre='Fernando'; 
+
+
 SELECT * FROM alumno; 
-UPDATE alumno SET f_nac='1990-04-20' WHERE nombre='nombre del alumno'; 
-UPDATE alumno SET apellido='Alonso' WHERE nombre='nombre del alumno'; 
-INSERT INTO alumno VALUES ('Fernando', 'Alonso', '1981-07-29'); 
 
-
-SHOW DATABASES;
+-- SHOW DATABASES;
 
