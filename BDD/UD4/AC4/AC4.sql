9012e7f30f98071e2c3a6ddecc76fe4041cb786a
COMPU-S040\daw1 <pavelmiron6824@gmail.com>
2025-04-11 11:21:40 +0200

    no message

--- BDD/UD4/AC4/AC4.sql
+++ BDD/UD4/AC4/AC4.sql
@@ -1,16 +1,33 @@
+DROP DATABASE IF EXISTS ud4_ac4;
+CREATE DATABASE IF NOT EXISTS ud4_ac4;
+USE ud4_ac4;
 
 
-
-
-
+create table clientes(
+	id			int auto_increment primary key,
+	first_name 	VARCHAR(40),
+    last_name	VARCHAR(40),
+    age 		int
+);
+-- 1
 DELIMITER $$
 CREATE TRIGGER check_age
 BEFORE INSERT ON clientes
 FOR EACH ROW
 BEGIN
-	IF NEW.age <0
+	IF NEW.age <16
 		THEN SIGNAL SQLSTATE '50001' SET MESSAGE_TEXT = 'Error. La edad debe ser
-	superior a 0';
+	superior a 16';
 	END IF;
 END $$
 
+-- 2
+DELIMITER $$
+CREATE TRIGGER check_name
+BEFORE INSERT ON clientes
+FOR EACH ROW
+BEGIN
+	IF NEW.first_name ='Lucia' or 'Martin' 
+		THEN SIGNAL SQLSTATE '50001' SET MESSAGE_TEXT = 'Error.';
+	END IF;
+END $$
