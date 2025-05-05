af28fecb829aa4dfd49a65cdcefddaa611ef9ae5
COMPU-S040\daw1 <pavelmiron6824@gmail.com>
2025-05-05 11:21:11 +0200

    no message

--- DAW/UD7/demoFX/src/main/java/org/example/demofx/MainApp.java
+++ DAW/UD7/demoFX/src/main/java/org/example/demofx/MainApp.java
@@ -12,7 +12,7 @@ public class MainApp extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 520, 440);
-        stage.setTitle("Bien venido!");
+        stage.setTitle("Bienvenido!");
         stage.setScene(scene);
         stage.show();
     }
