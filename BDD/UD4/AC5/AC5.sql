f3bfb76439b0810fb0944f9d2058ed36475860d3
COMPU-S040\daw1 <pavelmiron6824@gmail.com>
2025-05-16 11:10:38 +0200

    no message

--- BDD/UD4/AC5/AC5.sql
+++ BDD/UD4/AC5/AC5.sql
@@ -2,91 +2,128 @@ DROP DATABASE IF EXISTS ud4_ac5;
 CREATE DATABASE IF NOT EXISTS ud4_ac5;
 USE ud4_ac5;
 
-
-DROP TABLE IF EXISTS dept_emp,
-                     dept_manager,
-                     titles,
-                     salaries,
-                     employees,
-                     departments;
-
-
-CREATE TABLE employees (
-    emp_no      INT             NOT NULL,
-    birth_date  DATE            NOT NULL,
-    first_name  VARCHAR(14)     NOT NULL,
-    last_name   VARCHAR(16)     NOT NULL,
-    gender      ENUM ('M','F')  NOT NULL,    
-    hire_date   DATE            NOT NULL,
-    PRIMARY KEY (emp_no)
-);
-
-CREATE TABLE departments (
-    dept_no     CHAR(4)         NOT NULL,
-    dept_name   VARCHAR(40)     NOT NULL,
-    PRIMARY KEY (dept_no),
-    UNIQUE  KEY (dept_name)
-);
-
-CREATE TABLE dept_manager (
-   emp_no       INT             NOT NULL,
-   dept_no      CHAR(4)         NOT NULL,
-   from_date    DATE            NOT NULL,
-   to_date      DATE            NOT NULL,
-   FOREIGN KEY (emp_no)  REFERENCES employees (emp_no)    ON DELETE CASCADE,
-   FOREIGN KEY (dept_no) REFERENCES departments (dept_no) ON DELETE CASCADE,
-   PRIMARY KEY (emp_no,dept_no)
-);
-
-CREATE TABLE dept_emp (
-    emp_no      INT             NOT NULL,
-    dept_no     CHAR(4)         NOT NULL,
-    from_date   DATE            NOT NULL,
-    to_date     DATE            NOT NULL,
-    FOREIGN KEY (emp_no)  REFERENCES employees   (emp_no)  ON DELETE CASCADE,
-    FOREIGN KEY (dept_no) REFERENCES departments (dept_no) ON DELETE CASCADE,
-    PRIMARY KEY (emp_no,dept_no)
-);
-
-CREATE TABLE titles (
-    emp_no      INT             NOT NULL,
-    title       VARCHAR(50)     NOT NULL,
-    from_date   DATE            NOT NULL,
-    to_date     DATE,
-    FOREIGN KEY (emp_no) REFERENCES employees (emp_no) ON DELETE CASCADE,
-    PRIMARY KEY (emp_no,title, from_date)
-)
-;
-
-CREATE TABLE salaries (
-    emp_no      INT             NOT NULL,
-    salary      INT             NOT NULL,
-    from_date   DATE            NOT NULL,
-    to_date     DATE            NOT NULL,
-    FOREIGN KEY (emp_no) REFERENCES employees (emp_no) ON DELETE CASCADE,
-    PRIMARY KEY (emp_no, from_date)
-)	
-;
-
-
-CREATE TABLE dept_name_changes (
-    dept_no CHAR(4) NOT NULL,
-    old_name VARCHAR(40) NOT NULL,
-    new_name VARCHAR(40) NOT NULL,
-    change_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (dept_no, change_date),
-    FOREIGN KEY (dept_no) REFERENCES departments (dept_no) ON DELETE CASCADE
-);
-
+-- 1 
 DELIMITER $$
-create procedure suma (in num1 int,
-in num2 int, out resultado int)
+create procedure suma (
+	in num1 int,
+	in num2 int, 
+	out resultado int
+)
 begin 
 	set resultado = num1 + num2;
 END $$
-
+--
 call suma (2, 2, @resultado);
-
 select @resultado;
+
+
+
+
+
+
+
+-- 2
+DELIMITER $$
+create procedure procedimiento (
+	in num1 int,
+	inout num2 int
+)
+begin 
+
+	select num1 + num2 AS suma;
+	set num2 = num1 * num2;
+    
+END $$
+--
+set @num2 = 5;
+call procedimiento (3, @num2);
+select @num2 AS multiplicacion;
+
+
+
+
+
+
+-- 3
+-- DELIMITER $$
+-- create function funcion (	
+-- 	num1 int,
+-- 	num2 int 
+-- ) returns int
+-- deterministic
+-- begin 
+-- 	return resultado = num1 + num2;
+-- END $$
+-- --
+-- select funcion(7, 8) as resultado;
+
+
+
+
+
+
+
+
+
+
+-- 4
+DELIMITER $$
+create procedure precio_sin_iva(
+    in precio decimal(10,2),
+    out precio_sin_iva decimal(10,2)
+)
+begin 
+    set precio_sin_iva = precio / 1.21;
+END $$
+
+-- 
+call precio_sin_iva(121, @precio_sin_iva);
+select @precio_sin_iva AS precio_sin_iva;
+
+
+
+
+
+
+
+
+
+-- 5
+DELIMITER $$
+create function dia_semana (
+	num_dia int
+) returns varchar(20)
+deterministic
+begin
+	declare dia varchar(20);
+    
+    case num_dia
+		when 1 then set dia = 'lunes';
+        when 2 then set dia = 'martes';
+        when 3 then set dia = 'miercoles';
+        when 4 then set dia = 'jueves';
+        when 5 then set dia = 'viernes';
+        when 6 then set dia = 'sabado';
+        when 7 then set dia = 'domingo';
+        else set dia = 'invalido';
+	end case;
+    
+    return dia;
+END$$
+
+-- 
+select dia_semana(2) as dia;
+
+
+
+
+
+
+
+
+
+
+
+
 
 
