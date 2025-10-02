48865299a6143390c24879d352f8fb657e790e54
COMPUS040\daw2 <pavelmiron6824@gmail.com>
2025-10-02 21:02:14 +0200

    no message

---
+++ DAW2/DWES/UD1/Actividad2PHP/index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// 1 hago un array con nombres
+$nombres = ["Pavel", "Alex", "Ana", "Juan", "Pedro"];
+
+// 2 funcion para contar todos los caracteres
+function contarCaracteresTotales($nombres) {
+    $total = 0;
+    foreach ($nombres as $nombre) {
+        $total += strlen($nombre);
+    }
+    return $total;
+}
+
+echo "Total de caracteres: " . contarCaracteresTotales($nombres) . "<br>";
+
+// 3 funcion para contar cuantas veces aparece una letra
+function contarOcurrenciasDeLetra($nombres, $letra) {
+    $total = 0;
+    foreach ($nombres as $nombre) {
+        $total += substr_count(strtolower($nombre), strtolower($letra));
+    }
+    return $total;
+}
+
+$letra = "a";
+echo "La letra $letra aparece: " . contarOcurrenciasDeLetra($nombres, $letra) . "\n" . "<br>";
+
+// 4 funcion para contar vocales y consonantes
+function contarVocalesYConsonantes($nombres) {
+    $vocales = "aeiou";
+    $v = 0;
+    $c = 0;
+
+    foreach ($nombres as $nombre) {
+        $nombre = strtolower($nombre);
+        for ($i = 0; $i < strlen($nombre); $i++) {
+            $letra = $nombre[$i];
+            if (ctype_alpha($letra)) {
+                if (strpos($vocales, $letra) !== false) {
+                    $v++;
+                } else {
+                    $c++;
+                }
+            }
+        }
+    }
+    return ["vocales" => $v, "consonantes" => $c];
+}
+
+$res = contarVocalesYConsonantes($nombres);
+echo "Vocales: " . $res["vocales"] . "<br>";
+echo "Consonantes: " . $res["consonantes"] . "<br>";
+
+// 5 funcion para encontrar nombre mas largo y mas corto
+function encontrarNombreMasLargoYCorto($nombres) {
+    $largo = $nombres[0];
+    $corto = $nombres[0];
+
+    foreach ($nombres as $nombre) {
+        if (strlen($nombre) > strlen($largo)) {
+            $largo = $nombre;
+        }
+        if (strlen($nombre) < strlen($corto)) {
+            $corto = $nombre;
+        }
+    }
+    return ["largo" => $largo, "corto" => $corto];
+}
+
+$res2 = encontrarNombreMasLargoYCorto($nombres);
+echo "Nombre mas largo: " . $res2["largo"] . "<br>";
+echo "Nombre mas corto: " . $res2["corto"] . "<br>";
+
+
+
+
+?>
+
+</body>
+</html>
