5f3322318925d365e7ac7c4b97a14d57d419f8fa
COMPUS040\daw2 <pavelmiron6824@gmail.com>
2025-10-23 20:54:52 +0200

    no message

--- DAW2/DWES/UD3/GestionPistas/gestionpistas/src/main/java/com/gestion/gestionpistas/addPista.java
+++ DAW2/DWES/UD3/GestionPistas/gestionpistas/src/main/java/com/gestion/gestionpistas/addPista.java
@@ -1,37 +1,38 @@
 package com.gestion.gestionpistas;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import ch.qos.logback.core.model.Model;
 
 
 @Controller
 
 public class addPista {
     
-    private ArrayList<Pista> pistas = new ArrayList<>();
+    private ArrayList<Pista> listaPistas = new ArrayList<>();
     
-    @GetMapping ("/addPista")
-    public String formPista(Model model) {
-        return "index.html";
+    @GetMapping ("/")
+    public String formularioPista(Model model) {
+        model.addAttribute("pistas", listaPistas);
+        return "index";
     }
 
     @PostMapping("/addPista")
-     public String guardarPista(@ModelAttribute Pista pista, RedirectAttributes redirectAttributes) {
-       
-        System.out.println("Nombre: " + pista.getNombrePista());
-        System.out.println("Ubicación: " + pista.getHorasDisponibles());
-
-       
-        redirectAttributes.addFlashAttribute("mensaje", "Pista guardada con éxito");
-        return "index.html"; 
+    public String addPista (@RequestParam ("nombrePista") @RequestParam("horas")) {
+        
+        
+        return entity;
     }
+    
+
+   
     
     
 
