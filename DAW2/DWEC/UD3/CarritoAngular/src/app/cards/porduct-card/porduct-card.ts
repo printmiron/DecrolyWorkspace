0d21032e5da3db6c6128d91b69b7e3d217fe9b71
P\cloud <pavelmiron6824@gmail.com>
2025-11-03 19:05:37 +0100

    no message

---
+++ DAW2/DWEC/UD3/CarritoAngular/src/app/cards/porduct-card/porduct-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-porduct-card',
+  imports: [],
+  templateUrl: './porduct-card.html',
+  styleUrl: './porduct-card.css',
+})
+export class PorductCard {
+
+}
