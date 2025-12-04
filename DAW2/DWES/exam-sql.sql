f47f3cf01532e1259975d4837843c238a0731289
P\cloud <pavelmiron6824@gmail.com>
2025-12-04 11:59:00 +0100

    no message

--- DAW2/DWES/exam-sql.sql
+++ DAW2/DWES/exam-sql.sql
@@ -1,13 +1,48 @@
-create database if not exists exempleexam;
-USE  exempleexam;
+CREATE DATABASE IF NOT EXISTS exempleexam;
+USE exempleexam;
 
-CREATE TABLE Santander (
-id BIGINT AUTO_INCREMENT  PRIMARY KEY,
-fecha date ,
-max_temp decimal(4,2),
-min_temp decimal(4,2),
-media_temp decimal(4,2),
-condicion ENUM('LLUVIA', 'SOLEADO', 'NUBLADO', 'TORMENTA', 'NIEVE', 'GRANIZO', 'GALERNA' ),
-humedad int,
-velocidad int
+CREATE TABLE IF NOT EXISTS Pais (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) UNIQUE NOT NULL
 );
+
+
+
+CREATE TABLE IF NOT EXISTS Ciudad (
+    id VARCHAR(40) PRIMARY KEY,
+
+    pais_id BIGINT NOT NULL,
+    nombre VARCHAR(100) NOT NULL,
+
+    fecha DATE,
+    max_temp DECIMAL(4,2),
+    min_temp DECIMAL(4,2),
+    media_temp DECIMAL(4,2),
+
+    condicion ENUM(
+        'LLUVIA',
+        'SOLEADO',
+        'NUBLADO',
+        'TORMENTA',
+        'NIEVE',
+        'GRANIZO',
+        'GALERNA'
+    ),
+
+    humedad INT,
+    velocidad INT,
+
+    -- Relación con Pais
+    CONSTRAINT fk_pais
+        FOREIGN KEY (pais_id) REFERENCES Pais(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+
+
+
+
+
+
+
